19f3ffef4c9feb9746e6f581cfa5787bb513b0df
dmitrii <dmitriibusiness@gmail.com>
2018-05-10 01:41:45 +0300

    Jar build. RC1

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -1,6 +1,9 @@
 package restaurant;
 
+import restaurant.view.Administrator;
 import restaurant.view.Authorization;
+
+import javax.swing.*;
 
 /**
  * Application entry point.
@@ -12,6 +15,24 @@ public class Restaurant {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /* Set the Nimbus look and feel */
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Administrator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Administrator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Administrator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Administrator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
         Authorization authorization = new Authorization();
         
         authorization.setVisible(true);
